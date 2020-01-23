fb02cd479563de906e66dd3673eac3b8d7a1d4cc
elykauf <emkaufm1@asu.edu>
2020-01-23 02:26:26 -0700

    initial deploy

---
+++ precache-manifest.71c9a8dd8dd4178cf6a6a8684312740b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db1b124127d5c001c89308fd768aa674",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "d7bb69fc415ee4c344e7",
+    "url": "/portfolio/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "dd02639d6fff1708ae48",
+    "url": "/portfolio/static/js/2.201fe0c9.chunk.js"
+  },
+  {
+    "revision": "13425e2e45fee351b4139959073d7e4f",
+    "url": "/portfolio/static/js/2.201fe0c9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d7bb69fc415ee4c344e7",
+    "url": "/portfolio/static/js/main.4ff7c94f.chunk.js"
+  },
+  {
+    "revision": "16c109c584451046bd84",
+    "url": "/portfolio/static/js/runtime-main.860ade9b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
